021bf7d56d0d075133ef8787e802bec44d950e79
cyf6l <1337295614@qq.com>
2019-01-22 10:01:51 +0800

    update first

--- src/github_test/github_test.java
+++ src/github_test/github_test.java
@@ -5,6 +5,7 @@ public class github_test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("hello world!!!");
+          System.out.println("update first");
 	}
 
 }
